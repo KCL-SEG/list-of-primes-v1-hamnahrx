b43a75613d9bd9d4ac0c2eec658739ede4bc233c
“HAMNAH <hamnah.1.rassen@kcl.ac.uk>
2023-11-11 19:43:55 +0000

    Update primes.py
    Implemented functions to generate prime numbers

--- primes.py
+++ primes.py
@@ -1,6 +1,28 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+#method to calculate prime numbers
+# for i in range of count, calculate prime number and add to list
+
+#function called is_prime
+# takes single parameter 'num'
+#
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
 def primes(number_of_primes):
     list = []
+    #count = int(input("Enter an integer: "))
+    count = 2
+
+    while len(list) < number_of_primes:
+        if is_prime(count):
+            list.append(count)
+        count += 1
+
     return list
